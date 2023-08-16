2149572948893d359989a9b1a444f79e92202a00
karimAlatif <themoonisnice@hotmail.com>
2023-08-16 15:56:49 +0300

    init

---
+++ src/pages/home/view/CDCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../../logo.svg';
+import '../../../App.css';
+
+function CDCard() {
+    return (
+        <div >
+            {"CDCard"}
+        </div>
+    );
+}
+
+export default CDCard;
